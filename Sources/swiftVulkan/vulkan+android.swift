1bdb36ecaa0be7a9707ae3647b82b3f6a622feb7
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-04-21 13:46:30 -0700

    Add Android surface creation from window support.

---
+++ Sources/swiftVulkan/vulkan+android.swift
@@ -0,0 +1,25 @@
+import vulkan
+import Foundation
+
+#if os(Android)
+
+import vulkan.android
+
+extension VulkanInstance {
+    public func createAndroidSurface(window: OpaquePointer) -> VkSurfaceKHR {
+        var androidSurfaceCreateInfo = VkAndroidSurfaceCreateInfoKHR()
+
+        androidSurfaceCreateInfo.sType = VK_STRUCTURE_TYPE_ANDROID_SURFACE_CREATE_INFO_KHR
+        androidSurfaceCreateInfo.window = window
+
+        var surface: VkSurfaceKHR? = nil
+
+        guard vkCreateAndroidSurfaceKHR(self.getInstance(), &androidSurfaceCreateInfo, nil, &surface) == VK_SUCCESS else {
+            preconditionFailure()
+        }
+
+        return surface!
+    }
+}
+
+#endif

bb491f936f38624cdb0ac52a34cdf957e2739543
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-04-22 20:43:32 -0700

    Add VulkanDescriptorSetLayout binding interface.

--- Sources/swiftVulkan/vulkan.swift
+++ Sources/swiftVulkan/vulkan.swift
@@ -384,6 +384,25 @@ public final class VulkanDescriptorSet {
     }
 }
 
+public final class VulkanDescriptorSetLayout {
+    private let device: VkDevice
+    private let descriptorSetLayout: VkDescriptorSetLayout
+
+    public init(device: VkDevice,
+                descriptorSetLayout: VkDescriptorSetLayout) {
+        self.device = device
+        self.descriptorSetLayout = descriptorSetLayout
+    }
+
+    deinit {
+        vkDestroyDescriptorSetLayout(self.device, self.descriptorSetLayout, nil)
+    }
+
+    public func getDescriptorSetLayout() -> VkDescriptorSetLayout {
+        return self.descriptorSetLayout
+    }
+}
+
 public final class VulkanDevice {
     private let device: VkDevice
 

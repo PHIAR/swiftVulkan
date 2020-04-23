d4bbfe106ee43a5c77d105bf5df61f520e42194c
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-04-22 20:38:35 -0700

    Add VulkanDescriptorSet binding interface.

--- Sources/swiftVulkan/vulkan.swift
+++ Sources/swiftVulkan/vulkan.swift
@@ -338,6 +338,33 @@ public final class VulkanCommandPool {
     }
 }
 
+public final class VulkanDescriptorSet {
+    private let device: VkDevice
+    private let descriptorPool: VkDescriptorPool
+    private let descriptorSet: VkDescriptorSet
+
+    public init(device: VkDevice,
+                descriptorPool: VkDescriptorPool,
+                descriptorSet: VkDescriptorSet) {
+        self.device = device
+        self.descriptorPool = descriptorPool
+        self.descriptorSet = descriptorSet
+    }
+
+    deinit {
+        var descriptorSet: VkDescriptorSet? = self.descriptorSet
+
+        vkFreeDescriptorSets(self.device,
+                             self.descriptorPool,
+                             1,
+                             &descriptorSet)
+    }
+
+    public func getDescriptorSet() -> VkDescriptorSet {
+        return self.descriptorSet
+    }
+}
+
 public final class VulkanDevice {
     private let device: VkDevice
 

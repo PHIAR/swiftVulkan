442f66fc928d0653d049af0fdb21106992aa5d33
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-04-22 19:58:29 -0700

    Add bind buffer to device memory implementation and API.

--- Sources/swiftVulkan/vulkan.swift
+++ Sources/swiftVulkan/vulkan.swift
@@ -15,6 +15,16 @@ public final class VulkanBuffer {
         vkDestroyBuffer(self.device, self.buffer, nil)
     }
 
+    public func bindBufferMemory(deviceMemory: VulkanDeviceMemory,
+                                 offset: Int) {
+        guard vkBindBufferMemory(self.device,
+                                 self.buffer,
+                                 deviceMemory.getDeviceMemory(),
+                                 VkDeviceSize(offset)) == VK_SUCCESS else {
+            preconditionFailure()
+        }
+    }
+
     public func getBuffer() -> VkBuffer {
         return self.buffer
     }

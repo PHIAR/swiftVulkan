253fbae5b0b8251482d245e59c833d612c4b66a6
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-04-28 01:54:02 -0700

    Add specialization constant forwarding to pipeline stage creation.

--- Sources/swiftVulkan/VulkanPipelineShaderStage.swift
+++ Sources/swiftVulkan/VulkanPipelineShaderStage.swift
@@ -6,15 +6,45 @@ public final class VulkanPipelineShaderStage {
     private let stage: VkShaderStageFlagBits
     private let shaderModule: VulkanShaderModule
     private let name: UnsafeMutablePointer <CChar>
+    private let specializationConstants: [VkSpecializationMapEntry]
+    private let _specializationConstants: UnsafeBufferPointer <VkSpecializationMapEntry>
+    private let specializationData: UnsafeRawBufferPointer?
+    private let specializationInfo: [VkSpecializationInfo]
+    private let _specializationInfo: UnsafeBufferPointer <VkSpecializationInfo>?
 
     public init(flags: VkPipelineShaderStageCreateFlags = 0,
                 stage: VkShaderStageFlagBits,
                 shaderModule: VulkanShaderModule,
-                name: String) {
+                name: String,
+                specializationConstants: [VkSpecializationMapEntry] = [],
+                specializationData: UnsafeRawBufferPointer? = nil) {
+        let _specializationConstants = specializationConstants.withUnsafeBytes { UnsafeBufferPointer(start: $0.baseAddress!.assumingMemoryBound(to: VkSpecializationMapEntry.self ),
+                                                                                                     count: specializationConstants.count) }
+        var specializationInfo = VkSpecializationInfo()
+
+        specializationInfo.mapEntryCount = UInt32(specializationConstants.count)
+        specializationInfo.pMapEntries = _specializationConstants.baseAddress!
+        specializationInfo.dataSize = specializationData?.count ?? 0
+        specializationInfo.pData = specializationData?.baseAddress
+
         self.flags = flags
         self.stage = stage
         self.shaderModule = shaderModule
         self.name = name.withCString { strdup($0) }
+        self.specializationConstants = specializationConstants
+        self._specializationConstants = _specializationConstants
+        self.specializationData = specializationData
+
+        if specializationConstants.isEmpty {
+            self.specializationInfo = []
+            self._specializationInfo = nil
+        } else {
+            let _specializationInfo = [ specializationInfo ]
+
+            self.specializationInfo = _specializationInfo
+            self._specializationInfo = _specializationInfo.withUnsafeBytes { UnsafeBufferPointer(start: $0.baseAddress!.assumingMemoryBound(to: VkSpecializationInfo.self ),
+                                                                                                 count: 1) }
+        }
     }
 
     deinit {
@@ -28,6 +58,7 @@ public final class VulkanPipelineShaderStage {
         pipelineShaderStageCreateInfo.stage = self.stage
         pipelineShaderStageCreateInfo.module = self.shaderModule.getShaderModule()
         pipelineShaderStageCreateInfo.pName = UnsafeRawPointer(self.name).assumingMemoryBound(to: CChar.self)
+        pipelineShaderStageCreateInfo.pSpecializationInfo = self._specializationInfo?.baseAddress
         return pipelineShaderStageCreateInfo
     }
 }

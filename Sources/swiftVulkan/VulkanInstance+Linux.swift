3cd7572e58346b9c520794767ebc14f28fee233a
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-04-30 14:57:59 -0700

    Add support for linux native surfaces.

---
+++ Sources/swiftVulkan/VulkanInstance+Linux.swift
@@ -0,0 +1,46 @@
+import vulkan
+import Foundation
+
+#if os(Linux)
+
+import vulkan.linux
+
+extension VulkanInstance {
+    public func createXcbSurface(connection: OpaquePointer,
+                                 window: xcb_window_t) -> VulkanSurface {
+        var xcbSurfaceCreateInfo = VkXcbSurfaceCreateInfoKHR()
+
+        xcbSurfaceCreateInfo.sType = VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR
+        xcbSurfaceCreateInfo.connection = connection
+        xcbSurfaceCreateInfo.window = window
+
+        var surface: VkSurfaceKHR? = nil
+
+        guard vkCreateXcbSurfaceKHR(self.getInstance(), &xcbSurfaceCreateInfo, nil, &surface) == VK_SUCCESS else {
+            preconditionFailure()
+        }
+
+        return VulkanSurface(instance: self.getInstance(),
+                             surface: surface!)
+    }
+
+    public func createXlibSurface(display: OpaquePointer,
+                                  window: Window) -> VulkanSurface {
+        var xlibSurfaceCreateInfo = VkXlibSurfaceCreateInfoKHR()
+
+        xlibSurfaceCreateInfo.sType = VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR
+        xlibSurfaceCreateInfo.dpy = display
+        xlibSurfaceCreateInfo.window = window
+
+        var surface: VkSurfaceKHR? = nil
+
+        guard vkCreateXlibSurfaceKHR(self.getInstance(), &xlibSurfaceCreateInfo, nil, &surface) == VK_SUCCESS else {
+            preconditionFailure()
+        }
+
+        return VulkanSurface(instance: self.getInstance(),
+                             surface: surface!)
+    }
+}
+
+#endif
---
+++ Sources/vulkan/vulkan_linux.h
@@ -0,0 +1,4 @@
+#include <xcb/xcb.h>
+#include <X11/Xlib.h>
+#include <vulkan/vulkan_xcb.h>
+#include <vulkan/vulkan_xlib.h>

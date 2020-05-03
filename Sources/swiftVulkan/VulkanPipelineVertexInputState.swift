96cc64aab1c37e11642a1fc6aa6b79fcbe1b359b
Aaditya Chandrasekhar <aadityac@gmail.com>
2020-05-03 15:14:43 -0700

    Add implementation for VulkanPipelineVertexInputState.

--- Sources/swiftVulkan/VulkanPipelineVertexInputState.swift
+++ Sources/swiftVulkan/VulkanPipelineVertexInputState.swift
@@ -2,13 +2,27 @@ import vulkan
 import Foundation
 
 public final class VulkanPipelineVertexInputState {
-    public init() {
+    private let attributes: [VkVertexInputAttributeDescription]
+    private let bindings: [VkVertexInputBindingDescription]
+
+    public init(attributes: [VkVertexInputAttributeDescription],
+                bindings: [VkVertexInputBindingDescription]) {
+        self.attributes = attributes
+        self.bindings = bindings
     }
 
     public func getPipelineVertexInputStateCreateInfo() -> VkPipelineVertexInputStateCreateInfo {
-        var pipelineVertexInputStateCreateInfo = VkPipelineVertexInputStateCreateInfo()
+        return self.attributes.withUnsafeBytes { _attributes in
+            self.bindings.withUnsafeBytes { _bindings in
+                var pipelineVertexInputStateCreateInfo = VkPipelineVertexInputStateCreateInfo()
 
-        pipelineVertexInputStateCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO
-        return pipelineVertexInputStateCreateInfo
+                pipelineVertexInputStateCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO
+                pipelineVertexInputStateCreateInfo.vertexBindingDescriptionCount = UInt32(self.attributes.count)
+                pipelineVertexInputStateCreateInfo.pVertexBindingDescriptions = _attributes.baseAddress!.assumingMemoryBound(to: VkVertexInputBindingDescription.self)
+                pipelineVertexInputStateCreateInfo.vertexAttributeDescriptionCount = UInt32(self.bindings.count)
+                pipelineVertexInputStateCreateInfo.pVertexAttributeDescriptions = _bindings.baseAddress!.assumingMemoryBound(to: VkVertexInputAttributeDescription.self)
+                return pipelineVertexInputStateCreateInfo
+            }
+        }
     }
 }
